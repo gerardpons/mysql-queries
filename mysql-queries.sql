8a8c29a1b3ce394ebfd70862a691a65c93ded787
Gerard Pons <gpguisado@gmail.com>
2021-12-15 09:42:23 +0100

    Queries Universidad + unificar queries

--- mysql-queries.sql
+++ mysql-queries.sql
@@ -1,3 +1,7 @@
+-- BASE DE DADES TIENDA
+
+USE tienda;
+
 SELECT nombre FROM producto;
 SELECT nombre, precio FROM producto;
 SELECT * FROM producto;
@@ -39,3 +43,41 @@ SELECT p.nombre, MAX(p.precio) FROM producto p JOIN fabricante f ON p.codigo_fab
 SELECT p.nombre, MIN(p.precio) FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE f.nombre = 'Hewlett-Packard';
 SELECT * FROM producto p WHERE p.precio >= (SELECT MAX(p.precio) FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE f.nombre = 'Lenovo');
 SELECT * FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE f.nombre = 'Asus' AND p.precio > (SELECT AVG(p.precio) FROM producto p JOIN fabricante f ON p.codigo_fabricante = f.codigo WHERE f.nombre = 'Asus');
+
+
+-- BASE DE DADES UNIVERSIDAD
+
+USE universidad;
+
+SELECT apellido1, apellido2, nombre FROM persona p WHERE tipo = 'alumno' ORDER BY apellido1 ASC, apellido2 ASC, nombre ASC;
+SELECT nombre, apellido1, apellido2 FROM persona p WHERE tipo = 'alumno' AND telefono IS NULL;
+SELECT nombre, apellido1, apellido2 FROM persona p WHERE tipo = 'alumno' AND fecha_nacimiento REGEXP '1999';
+SELECT nombre, apellido1, apellido2 FROM persona p WHERE tipo ='profesor' AND telefono IS NULL AND nif REGEXP 'K$';
+SELECT nombre FROM asignatura WHERE id_grado = 7 AND curso = 3 AND cuatrimestre = 1;
+SELECT pa.apellido1, pa.apellido2, pa.nombre, dp.nombre FROM persona pa JOIN profesor pr ON pa.id = pr.id_profesor JOIN departamento dp ON pr.id_departamento = dp.id ORDER BY pa.apellido1 ASC, pa.apellido2 ASC, pa.nombre ASC;
+SELECT a.nombre, ce.anyo_inicio, ce.anyo_fin FROM alumno_se_matricula_asignatura asma JOIN persona pa ON asma.id_alumno = pa.id JOIN curso_escolar ce ON asma.id_curso_escolar = ce.id JOIN asignatura a ON asma.id_asignatura = a.id WHERE pa.nif = '26902806M';
+SELECT DISTINCT(d.nombre) FROM departamento d JOIN profesor pr ON pr.id_departamento = d.id JOIN asignatura a ON pr.id_profesor = a.id_profesor WHERE a.id_grado = 4;
+SELECT DISTINCT(pa.nombre) FROM persona pa JOIN alumno_se_matricula_asignatura asma ON pa.id = asma.id_alumno JOIN curso_escolar ce ON ce.id = asma.id_curso_escolar JOIN asignatura a ON a.id = asma.id_asignatura WHERE ce.id = 5;
+
+-- Resolgui les 6 següents consultes utilitzant les clàusules LEFT JOIN i RIGHT JOIN.
+
+SELECT dp.nombre, pa.apellido1, pa.apellido2, pa.nombre FROM departamento dp LEFT JOIN profesor pr ON dp.id = pr.id_departamento LEFT JOIN persona pa ON pr.id_profesor = pa.id ORDER BY dp.nombre ASC, pa.apellido1 ASC, pa.apellido2 ASC, pa.nombre ASC;
+SELECT * FROM profesor p WHERE p.id_departamento IS NULL;
+SELECT * FROM departamento dp LEFT JOIN profesor pr ON dp.id = pr.id_departamento WHERE id_profesor IS NULL;
+SELECT p.id_profesor FROM profesor p LEFT JOIN asignatura a ON p.id_profesor = a.id_profesor WHERE a.id_profesor IS NULL;
+SELECT a.nombre FROM asignatura a LEFT JOIN profesor p ON a.id_profesor = p.id_profesor WHERE a.id_profesor IS NULL;
+SELECT DISTINCT dp.nombre FROM departamento dp LEFT JOIN profesor pr ON dp.id = pr.id_departamento LEFT JOIN asignatura a ON pr.id_profesor = a.id_profesor LEFT JOIN alumno_se_matricula_asignatura asma ON a.id = asma.id_asignatura WHERE a.id IS NULL;
+
+-- Consultes resum:
+
+SELECT COUNT(*) FROM persona WHERE tipo = 'alumno';
+SELECT COUNT(*) FROM persona WHERE tipo = 'alumno' AND fecha_nacimiento REGEXP '^1999';
+SELECT COUNT(*) FROM departamento dp JOIN profesor pf ON dp.id = pf.id_departamento;
+SELECT dp.nombre, COUNT(*) FROM departamento dp LEFT JOIN profesor pf ON dp.id = pf.id_departamento GROUP BY dp.nombre;
+SELECT g.nombre, COUNT(*) FROM asignatura a LEFT JOIN grado g ON a.id_grado = g.id GROUP BY g.nombre ORDER BY COUNT(*);
+SELECT g.nombre, COUNT(*) FROM asignatura a LEFT JOIN grado g ON a.id_grado = g.id GROUP BY g.nombre ORDER BY COUNT(*) DESC LIMIT 1;
+SELECT g.nombre, a.tipo, COUNT(a.creditos) FROM asignatura a LEFT JOIN grado g ON a.id_grado = g.id GROUP BY a.nombre;
+SELECT ce.anyo_inicio, COUNT(asma.id_alumno) FROM curso_escolar ce LEFT JOIN alumno_se_matricula_asignatura asma ON ce.id = asma.id_curso_escolar GROUP BY ce.anyo_inicio;
+SELECT pa.id, pa.nombre, pa.apellido1, pa.apellido2, COUNT(a.id) FROM profesor pf LEFT JOIN persona pa ON pf.id_profesor = pa.id LEFT JOIN asignatura a ON a.id_profesor = pf.id_profesor GROUP BY pa.id ORDER BY COUNT(a.id);
+SELECT * FROM persona WHERE tipo = 'alumno' AND fecha_nacimiento = (SELECT MAX(fecha_nacimiento) FROM persona WHERE tipo = 'alumno');
+SELECT * FROM asignatura a LEFT JOIN profesor pf ON pf.id_profesor = a.id_profesor WHERE id_departamento IS NOT NULL AND a.id_profesor IS NULL;
